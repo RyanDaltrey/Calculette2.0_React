030ae667c24db9d030fb625ff68764c4dabc4134
Ryan <ryandaltrey@hotmail.com>
2024-03-28 14:40:30 +0100

    Ajout de la function clickOnButton et de son appel a la fonction onClick

--- src/Calculette.jsx
+++ src/Calculette.jsx
@@ -1,43 +1,46 @@
 import React from "react";
 
 const Calculette = () => {
-  return (
-    <>
+
+    const clickOnButton = (e) => {
+        console.log(e.target.innerHTML);
+    }
+ 
+  return <>
     <div id="block_calculatrice">
       <div id="barre_total">
         <input id="input_total" type="text" />
       </div>
       <div id="premiere_ligne">
-        <button className="boutton_ce">CE</button>
-        <button className="boutton_symbole">%</button>
-        <button className="boutton_symbole">+</button>
-        <button className="boutton_symbole">-</button>
+        <button className="boutton_ce" onClick={clickOnButton}>CE</button>
+        <button className="boutton_symbole" onClick={clickOnButton}>%</button>
+        <button className="boutton_symbole" onClick={clickOnButton}>+</button>
+        <button className="boutton_symbole" onClick={clickOnButton}>-</button>
       </div>
       <div id="deuxieme_ligne">
-        <button className="boutton_chiffre">7</button>
-        <button className="boutton_chiffre">8</button>
-        <button className="boutton_chiffre">9</button>
-        <button className="boutton_symbole">/</button>
+        <button className="boutton_chiffre" onClick={clickOnButton}>7</button>
+        <button className="boutton_chiffre" onClick={clickOnButton}>8</button>
+        <button className="boutton_chiffre" onClick={clickOnButton}>9</button>
+        <button className="boutton_symbole" onClick={clickOnButton}>/</button>
       </div>
       <div id="troisieme_ligne">
-        <button className="boutton_chiffre">4</button>
-        <button className="boutton_chiffre">5</button>
-        <button className="boutton_chiffre">6</button>
-        <button className="boutton_symbole">*</button>
+        <button className="boutton_chiffre" onClick={clickOnButton}>4</button>
+        <button className="boutton_chiffre" onClick={clickOnButton}>5</button>
+        <button className="boutton_chiffre" onClick={clickOnButton}>6</button>
+        <button className="boutton_symbole" onClick={clickOnButton}>*</button>
       </div>
       <div id="quatrieme_ligne">
-        <button className="boutton_chiffre">1</button>
-        <button className="boutton_chiffre">2</button>
-        <button className="boutton_chiffre">3</button>
-        <button className="boutton_symbole">=</button>
+        <button className="boutton_chiffre" onClick={clickOnButton}>1</button>
+        <button className="boutton_chiffre" onClick={clickOnButton}>2</button>
+        <button className="boutton_chiffre" onClick={clickOnButton}>3</button>
+        <button className="boutton_symbole" onClick={clickOnButton}>=</button>
       </div>
       <div id="cinquieme_ligne">
-        <button className="boutton_chiffre">0</button>
-        <button className="boutton_symbole">.</button>
+        <button className="boutton_chiffre" onClick={clickOnButton}>0</button>
+        <button className="boutton_symbole" onClick={clickOnButton}>.</button>
       </div>
       </div>
     </>
-  );
 };
 
 export default Calculette;
